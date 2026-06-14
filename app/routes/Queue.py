c6cd826e060bcaef46f54de399b23019c377ab02
R-zin <razinm24bcs24@iiitkottayam.ac.in>
2026-06-14 21:57:47 +0530

    Listing redis queue features

--- app/routes/Queue.py
+++ app/routes/Queue.py
@@ -1,12 +1,17 @@
-
+import redis
 from fastapi import APIRouter,HTTPException
 from .. models.models import list_queue_out
+from redis import Redis
+r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 router = APIRouter()
 
 @router.get("/queues",response_model=list_queue_out)
 async def list_queues():
     try:
-        pass
+        res = []
+        for key in r.scan_iter(_type="LIST"):
+            res.append({key:r.llen(key)})
+        return {"queues":res}
     except Exception as e:
         raise HTTPException(status_code=500,detail=str(e))
 
