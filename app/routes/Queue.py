23f90a59f0e4256eec35d5c72d38ec49114f5cf9
R-zin <razinm24bcs24@iiitkottayam.ac.in>
2026-06-15 23:15:41 +0530

    Complete Queue Operation of Redis:

--- app/routes/Queue.py
+++ app/routes/Queue.py
@@ -1,9 +1,12 @@
 import redis
 from fastapi import APIRouter,HTTPException
+from fastapi import Depends
 from .. models.models import list_queue_out
 from redis import Redis
 r = redis.Redis(host='localhost', port=6379, decode_responses=True)
 router = APIRouter()
+
+def admin_route()
 
 @router.get("/queues",response_model=list_queue_out)
 async def list_queues():
@@ -15,17 +18,19 @@ async def list_queues():
     except Exception as e:
         raise HTTPException(status_code=500,detail=str(e))
 
-@router.get("/queues/{queue_name}")
+@router.get("/queues/{queue_name}") #Fetching the tasks pending in Queue
 async def get_stats_queue(queue_name:str):
     try:
-        pass
+        res = r.lrange(queue_name, 0, -1)
+        return {"items":res}
     except Exception as e:
         raise HTTPException(status_code=500,detail=str(e))
 
 @router.delete("/queues/{queue_name}")
 async def purge(queue_name:str):  #Make this admin only
     try:
-        pass
+        await r.unlink(queue_name)
+        return {"Status":"Successfully purged"}
     except Exception as e:
         raise HTTPException(status_code=500,detail=str(e))
 
