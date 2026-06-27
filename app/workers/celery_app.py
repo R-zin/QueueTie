bb8dbf992718d55c223f3be8f7b5c0f6b21e43ca
R-zin <razinm24bcs24@iiitkottayam.ac.in>
2026-06-27 16:01:45 +0530

    Added database config file

--- app/workers/celery_app.py
+++ app/workers/celery_app.py
@@ -1,4 +1,7 @@
 import os
+
+from celery.app import autoretry
+import asyncio
 from email_worker import EmailWorker
 from celery import Celery
 from telegram_worker import TelegramWorker
@@ -6,17 +9,17 @@ REDIS_URL = os.environ.get("REDIS_URL")
 
 app = Celery("celery_app",broker=REDIS_URL,backend=REDIS_URL)
 
-@app.task
-async def send_email(to,subject,body):
+@app.task(bind=True,autoretry_for=(Exception,),retry_backoff=True,max_retries=5)
+def send_email(to,subject,body):
     try:
         c = EmailWorker()
-        await c.send_email(to,subject,body)
+        asyncio.run(c.send_email(to,subject,body))
     except Exception as e:
         raise Exception(e)
 @app.task
-async def send_sms(to,subject,body):
+def send_sms(to,subject,body):
     try:
-        await TelegramWorker.send_message(to,subject,body)
+        asyncio.run(TelegramWorker.send_message(to,subject,body))
     except Exception as e:
         raise Exception(e)
 
--- app/workers/database.py
+++ app/workers/database.py
@@ -1,2 +1,13 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = DeclarativeBase()
