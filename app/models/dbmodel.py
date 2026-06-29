068e6727e33566487926b9edeed64868ab594acd
R-zin <razinm24bcs24@iiitkottayam.ac.in>
2026-06-29 19:58:33 +0530

    add: postgresql db model for job

---
+++ app/models/dbmodel.py
@@ -0,0 +1,20 @@
+from workers.database import Base
+from sqlalchemy import Column, Integer, String,DateTime,Enum
+from uuid import uuid4
+import enum
+
+class JobStatus(str,enum.Enum):
+    RUNNING = "RUNNING"
+    SUCCESS = "SUCCESS"
+    FAILED = "FAILED"
+
+class Job(Base):
+    __tablename__ = "jobs"
+    id = Column(String, primary_key=True)
+    time_start = Column(DateTime)
+    status = Column(String)
+    time_end = Column(DateTime,nullable=True)
+    job_status = Column(Enum(JobStatus, name="job_status"))
+
+
+
